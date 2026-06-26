5dacc76cfa909235c9e79f37ef8b09686d93378c
Priyadarshini <priyadarshiniks83@gmail.com>
2026-06-26 21:05:00 +0530

    Update app for Render deployment

--- app.py
+++ app.py
@@ -88,5 +88,7 @@ def contact():
     return redirect(url_for("index", msg="Thanks for reaching out!"))
 
 # --- run ---
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
